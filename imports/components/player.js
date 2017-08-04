1b3ef1371ab3807e8ac71c8ac33040a452221306
kudou-reira <h.i.y.e.o.XD@gmail.com>
2017-08-05 08:15:35 +0900

    rename function added

--- imports/components/player.js
+++ imports/components/player.js
@@ -6,9 +6,13 @@ class Player extends Component{
     constructor(props){
         super(props);
 
-//        this.increment.bind(this);
-//        this.decrement.bind(this);
-//        this.remove.bind(this);
+        this.state = {
+            editName: false,
+            tempId: ''
+        }
+
+        this.handleSubmit.bind(this);
+        this.renameClick.bind(this);
     }
 
     increment = (id) => {
@@ -23,13 +27,46 @@ class Player extends Component{
         Players.remove({_id: id})
     }
 
+    handleSubmit = (e) => {
+        e.preventDefault();
+        //target is the form
+        let playerName = e.target.playerName.value;
+
+        // console.log(this.props.player._id);
+
+        Players.update({_id: this.props.player._id}, {$set: {name: playerName}});
+        this.setState({editName: !this.state.editName});
+    }
+
+    renameClick = () => {
+        this.setState({editName: !this.state.editName});
+    }
+
+    renderPlayerName = (id) => {
+
+        if(this.state.editName === false){
+            return(
+                <h3 className="player__name">{this.props.player.name}</h3>
+            );
+        }
+
+        else if(this.state.editName === true){
+            return(
+                <form className="form" onSubmit={this.handleSubmit}>
+                       <input className="form__input--rename" type="text" ref="singleName" name="playerName" placeholder="player name" defaultValue={this.props.player.name} />
+                       <button className="button--rename">Rename</button>
+                </form>
+            );
+        }
+    }
+
     render(){
         const itemAddOn = 'item item--position-' + this.props.player.rank;
         return(
             <div key={this.props.player._id} className={itemAddOn} >
                 <div className="player">
                     <div>
-                        <h3 className="player__name">{this.props.player.name}</h3>
+                        {this.renderPlayerName(this.props.player._id)}
                         <p className="player__stats">
                             {this.props.player.position} {this.props.player.score} point(s).
                         </p>
@@ -38,6 +75,7 @@ class Player extends Component{
                         <button className="button button--round" onClick={() => this.increment(this.props.player._id)}>+1</button>
                         <button className="button button--round"  onClick={() => this.decrement(this.props.player._id)}>-1</button>
                         <button className="button button--round" onClick={() => this.remove(this.props.player._id)}>X</button>
+                        <button className="button button--round" onClick={this.renameClick}>e</button>
                     </div>
                 </div>
             </div>
